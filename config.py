9929389bfcb23eba6fffeb7d8e36ef9803e01bf7
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2024-11-20 01:24:12 +0700

    Added flask db init and migrate

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-secret-key'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess-secret-key"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
