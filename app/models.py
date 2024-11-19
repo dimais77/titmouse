4c3fe9680d30921adc270f2893452d61b883b89b
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2024-11-20 02:20:47 +0700

    Upgrade db

--- app/models.py
+++ app/models.py
@@ -8,15 +8,9 @@ from app import db
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    first_name: so.Mapped[str] = so.mapped_column(sa.String(64))
-    second_name: so.Mapped[str] = so.mapped_column(sa.String(64))
-    age: so.Mapped[int] = so.mapped_column(sa.Integer)
-    parent: so.Mapped[str] = so.mapped_column(sa.String(64))
-    phone_number: so.Mapped[str] = so.mapped_column(sa.String(64))
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
-
 
     def __repr__(self):
         return "<User {}>".format(self.username)
