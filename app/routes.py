482efb1d9b114aaac00a2f31be7d03c99b1e7bc9
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2024-11-19 23:58:12 +0700

    Added login and url_for

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -6,11 +6,25 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Валя'}
-    return render_template('index.html', title='Главная', user=user)
+    user = {'username': 'Гость'}
+    posts = [
+{
+    'author': {'username': 'Валя'},
+    'body': 'Уверена, уже все слепили СНЕГОВИКА. Теперь нарисуем!!'
+},
+{
+    'author': {'username': 'Инна'},
+    'body': 'Дочь занимается в этой студии рисованием и пластилинографией. Очень нравится!'
+}
+]
+    return render_template('index.html', title='Главная', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
