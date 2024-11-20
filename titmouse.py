da93ac64bc91174e9346d5c7b568657a64a99880
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2024-11-20 09:21:34 +0700

    Updated titmouse.py

--- titmouse.py
+++ titmouse.py
@@ -1,9 +1,11 @@
+"""Module providing a import app and flask shell."""
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Post
 
 
-@app.shell_context_processor
+ """Function make shell context."""
+ @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
